d364c37e7a9cd12bafb52cb2dd56c5783537af40
yoshihiro okuda <yoshi.private.acc@gmail.com>
2019-01-23 20:14:33 +0900

    change layout and add param of channel

--- run.sh
+++ run.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+FOOTER="<https://github.com/cilly-yllic/wercker-step-slack|cilly/slack-notify>"
+DATE=$(date +%s)
+
 GIT_BRANCH_URL="https://$WERCKER_GIT_DOMAIN/$WERCKER_GIT_OWNER/$WERCKER_GIT_REPOSITORY"
 GIT_COMMIT_URL="$GIT_BRANCH_URL/commits/$WERCKER_GIT_COMMIT"
 
@@ -34,13 +37,6 @@ APPLICATION_LINK="<$WERCKER_RUN_URL|$WERCKER_APPLICATION_NAME>"
 GIT_BRANCH_LINK="<$GIT_BRANCH_URL|$WERCKER_GIT_BRANCH>"
 GIT_COMMIT_LINK="<$GIT_COMMIT_URL|$WERCKER_GIT_COMMIT>"
 
-MESSAGE="
-$ACTION_LINK for $APPLICATION_LINK $USER
-result: $WERCKER_RESULT
-branch: $GIT_BRANCH_LINK
-commit: $GIT_COMMIT_LINK
-"
-
 FALLBACK="
 $ACTION for $WERCKER_APPLICATION_NAME　$USER
 result: $WERCKER_RESULT
@@ -50,18 +46,44 @@ commit: $WERCKER_GIT_COMMIT
 
 COLOR="good"
 
+RESULT=$WERCKER_RESULT
 if [ "$WERCKER_RESULT" = "failed" ]; then
-  MESSAGE="$MESSAGE \n step: $WERCKER_FAILED_STEP_DISPLAY_NAME"
+  RESULT="$RESULT \n step: $WERCKER_FAILED_STEP_DISPLAY_NAME"
   FALLBACK="$FALLBACK \n step: $WERCKER_FAILED_STEP_DISPLAY_NAME"
   COLOR="danger"
 fi
 
 json="{
+    \"channel\": \"$WERCKER_SLACK_NOTIFY_CHANNEL\",
     \"attachments\":[
       {
         \"fallback\": \"$FALLBACK\",
-        \"text\": \"$MESSAGE\",
-        \"color\": \"$COLOR\"
+        \"author_name\": \"$WERCKER_STARTED_BY\",
+        \"fields\": [
+          {
+            \"title\": \"Type\",
+            \"value\": \"$ACTION_LINK\",
+          },
+          {
+            \"title\": \"Application\",
+            \"value\": \"$APPLICATION_LINK\",
+          },
+          {
+            \"title\": \"Result\",
+            \"value\": \"$RESULT\",
+          },
+          {
+            \"title\": \"Branch\",
+            \"value\": \"$GIT_BRANCH_LINK\",
+          },
+          {
+            \"title\": \"Commit\",
+            \"value\": \"$GIT_COMMIT_LINK\",
+          }
+        ],
+        \"color\": \"$COLOR\",
+        \"footer\": \"$FOOTER\",
+        \"ts\": \"$DATE\"
       }
     ]
 }"
